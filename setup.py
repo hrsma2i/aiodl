abecf9622c202f8cc0d915dce093bd850b780d23
hrsma2i <hrs.ma2i@gmail.com>
2020-01-01 12:16:02 +0900

    Change package manager: Pipenv -> poetry

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = ["aiodl"]
+
+package_data = {"": ["*"]}
+
+install_requires = [
+    "aiohttp>=3.6,<4.0",
+    "asyncio>=3.4,<4.0",
+    "numpy>=1.18,<2.0",
+    "pandas>=0.25.3,<0.26.0",
+    "pillow>=6.2,<7.0",
+    "tqdm>=4.41,<5.0",
+]
+
+setup_kwargs = {
+    "name": "aiodl",
+    "version": "0.1.0",
+    "description": "",
+    "long_description": None,
+    "author": "hrsma2i",
+    "author_email": "hrs.ma2i@gmail.com",
+    "url": None,
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.6,<4.0",
+}
 
 
-with open('./requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
-setup(
-    name="samplecli",
-    version="0.0.1",
-    description="A small package",
-    author="karakaram",
-    packages=find_packages(),
-    install_requires=install_requires,
-    entry_points={
-        "console_scripts": [
-            "aiodl=aiodl.download_img:main",
-        ]
-    },
-)
+setup(**setup_kwargs)
