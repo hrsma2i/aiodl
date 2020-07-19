0e02ce4c3e4c4372c1a835a2e54c67a589be3334
hrsma2i <hrs.ma2i@gmail.com>
2020-07-20 01:16:23 +0900

    Add Downloader.

--- aiodl/download_img.py
+++ aiodl/download_img.py
@@ -3,7 +3,6 @@
 
 import asyncio
 from datetime import datetime
-import os
 from logging import getLogger, INFO, StreamHandler
 from pathlib import Path
 
@@ -54,7 +53,7 @@ def main(
         help="Output directory",
     ),
     delimiter: str = typer.Option(",", "-d", help="Delimiter"),
-    n_requests: int = typer.Option(100, "-r", help="Number of requests"),
+    num_requests: int = typer.Option(100, "-r", help="Number of requests"),
     timeout: int = typer.Option(180, "-t", help="Timeout(sec)"),
     add: bool = typer.Option(False, "-a", help="Add files to the existing directory."),
 ):
@@ -76,50 +75,42 @@ def main(
     else:
         out_names = df[1].tolist()
 
-    # avoid to many requests(coros) the same time.
-    # limit them by setting semaphores (simultaneous requests)
-    sem = asyncio.Semaphore(n_requests)
-
+    downloader = Downloader(out_dir=out_dir, num_requests=num_requests, timeout=timeout)
     coros = [
-        download_file(url, out_dir, out_name, sem, timeout)
-        for url, out_name in zip(urls, out_names)
+        downloader.download(url, out_name) for url, out_name in zip(urls, out_names)
     ]
     eloop = asyncio.get_event_loop()
     eloop.run_until_complete(asyncio.wait(coros))
     eloop.close()
 
 
-async def download_file(url, out_dir, out_name, sem, timeout):
-    # this routine is protected by a semaphore
-    with await sem:
-        timeout_ = aiohttp.ClientTimeout(total=timeout)
-        content = await get(url, out_name, timeout=timeout_)
-        out_file = out_dir / out_name
+class Downloader:
+    def __init__(self, out_dir, num_requests, timeout):
+        self.out_dir = out_dir
+        # avoid to many requests(coros) the same time.
+        # limit them by setting semaphores (simultaneous requests)
+        self.sem = asyncio.Semaphore(num_requests)
+        self.timeout_ = aiohttp.ClientTimeout(total=timeout)
 
-        if content is not None:
-            write_to_file(out_file, content)
+    async def download(self, url, out_name):
+        # this routine is protected by a semaphore
+        with await self.sem:
+            content = await self.get(url, out_name, timeout=self.timeout_)
+            out_file = self.out_dir / out_name
 
+            if content is not None:
+                self.write(out_file, content)
 
-async def get(url, out_name, *args, **kwargs):
-    """a helper coroutine to perform GET requests:
-    """
-    async with aiohttp.ClientSession(raise_for_status=True) as session:
-        try:
-            async with session.get(url, *args, **kwargs) as res:
-                logger.info({"out_name": out_name})
-                return await res.content.read()
-        except Exception as e:
-            logger.error({"out_name": out_name, "error": e})
-            return None
+    async def get(self, url, out_name, *args, **kwargs):
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
+            try:
+                async with session.get(url, *args, **kwargs) as res:
+                    logger.info({"out_name": out_name})
+                    return await res.content.read()
+            except Exception as e:
+                logger.error({"out_name": out_name, "error": e})
+                return None
 
-
-def write_to_file(out_file, content):
-    """ get content and write it to file
-
-    Args:
-        out_file ([type]): [description]
-        content ([type]): [description]
-    """
-
-    with open(out_file, "wb") as f:
-        f.write(content)
+    def write(self, out_file: Path, content: bytes):
+        with out_file.open("wb") as f:
+            f.write(content)
